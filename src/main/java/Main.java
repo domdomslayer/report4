b39821644dc5af4899a9eeebbdea3f6b377e83a8
Hoshi Iju <e195713@ie.u-ryukyu.ac.jp>
2019-12-09 22:32:24 +0900

    fixed step3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        try{
-            String str = "百二十三";
-            int value = Integer.parseInt(str);
-        }catch(NullPointerException e){
-            System.out.println("エラー「NullPointerExeption」が発生しました。");
-            System.out.println(e.getMessage());
-        }
-
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
